f329b3ece761fc74c40cf5fd2df53be57b206bad
Cascao <matheusfranco23@hotmail.com>
2023-09-09 20:16:07 -0300

    structuring

--- src/index.ts
+++ src/index.ts
@@ -6,32 +6,36 @@ import puppeteer from "puppeteer";
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
-    })
+    });
 
     const page = await browser.newPage();
 
-    await page.goto("https://webmotors.com.br/carros/estoque/jeep/cherokee/", {
+    await page.goto("https://www.webmotors.com.br/carros/estoque/volkswagen/fusca/", {
         waitUntil: "domcontentloaded",
     });
-    setTimeout(async () => {
-        const selector: string = ".sc-GMQeP";
-    
-        const carsData = await page.$$eval(selector, items => {
-            return items.map(el => {
-                const nameElement = el.querySelector(".sc-hqyNC");
-                const priceElement = el.querySelector(".sc-cJSrbW");
-    
-                const name = nameElement ? nameElement.textContent?.trim() : "N/A";
-                const price = priceElement ? priceElement.textContent?.trim() : "N/A";
-    
-                return {
-                    name,
-                    price
-                };
-            });
-        });
-    
-        console.log(carsData);
-    }, 5000);
 
+    const selector = ".sc-GMQeP";
+
+    // Wait for the selector to be present in the DOM
+    await page.waitForSelector(selector);
+
+    const carsData = await page.$$eval(selector, items => {
+        return items.map(el => {
+            const nameElement = el.querySelector(".sc-hqyNC");
+            const priceElement = el.querySelector(".sc-cJSrbW");
+
+            const name = nameElement ? nameElement.textContent?.trim() : "N/A";
+            const price = priceElement ? priceElement.textContent?.trim() : "N/A";
+
+            return {
+                name,
+                price
+            };
+        });
+    });
+
+    console.log(carsData);
+
+    // Close the browser when done
+    await browser.close();
 })();
